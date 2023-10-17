4406cf419165831247466c0fb444cd0d80fc54b5
AlexZeroc <alexzeta220@gmail.com>
2023-10-17 09:41:36 +0300

    in useFetchSpecificTask and useFetchTaskList removed extra code

--- src/hooks/useFetchSpecificTask.js
+++ src/hooks/useFetchSpecificTask.js
@@ -1,13 +1,9 @@
 import TaskContext from "../store/task-list";
 
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
 export const useFetchSpecificTask = (id) => {
   const { setDataTask } = useContext(TaskContext);
-  const [dataDetail, setDataDetail] = useState(setDataTask);
-  useEffect(() => {
-    const resolve = setDataTask(id);
-    setDataDetail(resolve);
-  }, [id, setDataTask]);
-  return dataDetail;
+
+  return setDataTask(id);
 };
--- src/hooks/useFetchTaskList.js
+++ src/hooks/useFetchTaskList.js
@@ -1,14 +1,9 @@
 import TaskContext from "../store/task-list";
 
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
 export const useFetchTaskList = () => {
   const { arrayUserTask } = useContext(TaskContext);
-  const [data, setData] = useState(arrayUserTask);
-  useEffect(() => {
-    const resolve = arrayUserTask;
-    setData(resolve);
-  }, [setData, arrayUserTask]);
 
-  return data;
+  return arrayUserTask;
 };
