9121222aef7e5f85210f029e3017624730b078b7
AlexZeroc <alexzeta220@gmail.com>
2023-11-21 16:44:10 +0300

    test examples

--- src/App.test.js
+++ src/App.test.js
@@ -4,39 +4,46 @@ import TaskDetailsPage from './components/TaskDetailsPage/TaskDetailsPage';
 import { store } from './store/store';
 
 import { Provider } from 'react-redux';
+import nock from 'nock';
 
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+const routerRender = (url) => {
+  const routes = [
+    {
+      path: '/',
+      errorElement: <ErrorPage />,
+
+      children: [
+        {
+          index: true,
+          element: <TaskListPage />,
+        },
+        {
+          path: ':taskId',
+          element: <TaskDetailsPage />,
+        },
+      ],
+    },
+  ];
+  const router = createMemoryRouter(routes, {
+    initialEntries: [`${url}`],
+  });
+
+  return router;
+};
+const obj = nock('https://api.fake-rest.refine.dev').get('/');
+
+const myMock = jest.fn((x) => console.log(x, 'test'));
 
 describe('app components test', () => {
-  it('App render test', () => {
-    const routes = [
-      {
-        path: '/',
-        errorElement: <ErrorPage />,
-
-        children: [
-          {
-            index: true,
-            element: <TaskListPage />,
-          },
-          {
-            path: ':taskId',
-            element: <TaskDetailsPage />,
-          },
-        ],
-      },
-    ];
-
-    const router = createMemoryRouter(routes, {
-      initialEntries: ['/'],
-      initialIndex: 0,
-    });
+  it('App render test', async () => {
+    myMock(obj);
 
     // Arrange
     render(
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <RouterProvider router={routerRender('/')} />
       </Provider>
     );
   });
